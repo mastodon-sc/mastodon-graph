b4c791bf828b93015667df3018d4c27b78d7cd4f
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2016-10-18 14:29:30 +0200

    A breadth-first, cross component search.
    - search listener is notified when jumping from one component
    to another, but there is no edge in between in the search tree.
    - newly discovered components start with a depth of 0.
    - otherwise everything is as in the iterator counterpart.

---
+++ src/main/java/org/mastodon/graph/algorithm/traversal/BreadthFirstCrossComponentSearch.java
@@ -0,0 +1,45 @@
+package org.mastodon.graph.algorithm.traversal;
+
+import java.util.Iterator;
+
+import org.mastodon.graph.Edge;
+import org.mastodon.graph.ReadOnlyGraph;
+import org.mastodon.graph.Vertex;
+import org.mastodon.graph.algorithm.RootFinder;
+import org.mastodon.graph.traversal.AbstractBreadthFirstSearch;
+
+public class BreadthFirstCrossComponentSearch< V extends Vertex< E >, E extends Edge< V > > extends AbstractBreadthFirstSearch< BreadthFirstCrossComponentSearch< V, E >, V, E >
+{
+
+	private final Iterator< V > rit;
+
+	public BreadthFirstCrossComponentSearch( final ReadOnlyGraph< V, E > graph, final SearchDirection directed )
+	{
+		this( graph, directed, RootFinder.getRoots( graph ) );
+
+	}
+
+	public BreadthFirstCrossComponentSearch( final ReadOnlyGraph< V, E > graph, final SearchDirection directed, final Iterable< V > roots )
+	{
+		super( graph, directed );
+		this.rit = roots.iterator();
+	}
+
+	@Override
+	protected void visit( final V start )
+	{
+		super.visit( start );
+		while (rit.hasNext() )
+		{
+			final V next = rit.next();
+			if ( discovered.contains( next ) )
+				continue;
+
+			// When we jump to another root, we set its depth to 0.
+			depths.put( next, 0 );
+			queue.add( next );
+			searchListener.crossComponent( unqueued, next, this );
+			super.visit( next );
+		}
+	}
+}
