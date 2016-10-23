f59f6ff83463c0007cb16961af9495e23cf9baf5
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2016-10-23 18:03:10 +0200

    Minimal javadocs for interface AttributeUndoSerializer.

--- src/main/java/org/mastodon/undo/attributes/AttributeUndoSerializer.java
+++ src/main/java/org/mastodon/undo/attributes/AttributeUndoSerializer.java
@@ -21,23 +21,38 @@ public interface AttributeUndoSerializer< O >
 	public int getNumBytes();
 
 	/**
-	 * Store data from {@code obj} into {@code bytes}.
+	 * Stores data from {@code obj} into {@code bytes}.
+	 * <p>
+	 * The required array size can be obtained by {@link #getNumBytes()}.
+	 *
+	 * @param obj
+	 *            the object to store.
+	 * @param bytes
+	 *            the byte array in which to write.
 	 */
 	public void getBytes( final O obj, final byte[] bytes );
 
 	/**
-	 * Restore data from {@code bytes} into {@code obj}.
+	 * Restores data from {@code bytes} into {@code obj}.
+	 *
+	 * @param obj
+	 *            the object to restore.
+	 * @param bytes
+	 *            the byte array to read.
 	 */
 	public void setBytes( final O obj, final byte[] bytes );
 
 	/**
-	 * Notify that bytes have been written ({@link #setBytes(Object, byte[])})
+	 * Notifies that bytes have been written ({@link #setBytes(Object, byte[])})
 	 * to {@code obj}.
 	 * <p>
 	 * Note: Currently nothing is ever done in between {@code setBytes()} and
 	 * {@code notifySet()}, so maybe this will be removed later and
 	 * notifications directly linked to {@code setBytes()}. For now, we keep it
 	 * explicit.
+	 *
+	 * @param obj
+	 *            the object that has been modified.
 	 */
 	public void notifySet( final O obj );
 }
