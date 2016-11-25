c21fa47de930037995edcc4d933fbb281283440f
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2016-11-25 15:19:50 +0100

    JUnit BranchGraph test: link many vertices.

--- src/test/java/org/mastodon/graph/branch/BranchGraphTest.java
+++ src/test/java/org/mastodon/graph/branch/BranchGraphTest.java
@@ -1,10 +1,13 @@
 package org.mastodon.graph.branch;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mastodon.collection.RefCollections;
+import org.mastodon.collection.RefList;
 import org.mastodon.graph.GraphIdBimap;
 import org.mastodon.graph.ListenableTestEdge;
 import org.mastodon.graph.ListenableTestGraph;
@@ -109,7 +112,6 @@ public class BranchGraphTest
 		final ListenableTestVertex v1 = graph.addVertex().init( 1 );
 		final ListenableTestEdge e0 = graph.addEdge( v0, v1 ).init();
 		final ListenableTestVertex v2 = graph.addVertex().init( 2 );
-		System.out.println(); // DEBUG
 		final ListenableTestEdge e1 = graph.addEdge( v1, v2 ).init();
 
 		final PoolCollectionWrapper< BranchVertex > vertices = bg.vertices();
@@ -143,5 +145,69 @@ public class BranchGraphTest
 
 		final BranchEdge be1 = bg.getBranchEdge( e1, bg.edgeRef() );
 		assertEquals( "Non-join edge should link to the same branch edge.", be0, be1 );
+	}
+
+	@Test
+	public void testLinkManyVertices()
+	{
+		// First all vertices then all edges.
+		final RefList< ListenableTestVertex > vlist = RefCollections.createRefList( graph.vertices() );
+		for ( int i = 0; i < 5; i++ )
+			vlist.add( graph.addVertex().init( i ) );
+		final RefList< ListenableTestEdge > elist = RefCollections.createRefList( graph.edges() );
+		final ListenableTestVertex ref1 = graph.vertexRef();
+		final ListenableTestVertex ref2 = graph.vertexRef();
+		final ListenableTestEdge eref = graph.edgeRef();
+		for ( int i = 0; i < vlist.size() - 1; i++ )
+		{
+			final ListenableTestVertex source = vlist.get( i, ref1 );
+			final ListenableTestVertex target = vlist.get( i + 1, ref2 );
+			final ListenableTestEdge edge = graph.addEdge( source, target, eref ).init();
+			elist.add( edge );
+		}
+
+		final PoolCollectionWrapper< BranchVertex > vertices = bg.vertices();
+		final int vSize = vertices.size();
+		assertEquals( "Expected the branch graph to have 2 vertices.", 2, vSize );
+
+		final PoolCollectionWrapper< BranchEdge > edges = bg.edges();
+		final int eSize = edges.size();
+		assertEquals( "Expected the branch graph to have 1 edge.", 1, eSize );
+		
+		final ListenableTestVertex v0 = vlist.get( 0 );
+		final BranchVertex bv0 = bg.getBranchVertex( v0, bg.vertexRef() );
+		assertNotNull( "First linked vertex should link to a branch vertex.", bv0 );
+		assertEquals( "First branch vertex should link to first linked vertex in the branch.",
+				v0, bg.getLinkedVertex( bv0, graph.vertexRef() ) );
+
+		final ListenableTestVertex vlast = vlist.get( vlist.size() - 1 );
+		final BranchVertex bvlast = bg.getBranchVertex( vlast, bg.vertexRef() );
+		assertNotNull( "Last linked vertex should link to a branch vertex.", bvlast );
+		assertEquals( "Last branch vertex should link to last linked vertex in the branch.",
+				vlast, bg.getLinkedVertex( bvlast, graph.vertexRef() ) );
+
+		final BranchEdge be = bg.edges().iterator().next();
+		final ListenableTestEdge e0 = elist.get( 0 );
+		assertEquals( "Branch edge should link to first linked edge in the branch.",
+				e0, bg.getLinkedEdge( be, graph.edgeRef() ) );
+
+		
+		final BranchEdge beref = bg.edgeRef();
+		for ( int i = 1; i < vlist.size() - 1; i++ )
+		{
+			final ListenableTestVertex vi = vlist.get( i, ref1 );
+			assertNull( "Middle vertex should not link to a branch vertex.", bg.getBranchVertex( vi, bg.vertexRef() ) );
+			final BranchEdge bei = bg.getBranchEdge( vi, beref );
+			assertNotNull( "Middle vertex should link to a branch edge.", bei );
+			assertEquals( "Middle vertex shoud link to the branch edge.", be, bei );
+		}
+
+		for ( final ListenableTestEdge ei : elist )
+			assertEquals( "Linked edge should link to the branch edge.", be, bg.getBranchEdge( ei, beref ) );
+
+		graph.releaseRef( ref1 );
+		graph.releaseRef( ref2 );
+		graph.releaseRef( eref );
+		bg.releaseRef( beref );
 	}
 }
